feb3b9f77dddadaa616279d24029145ee4c8a354
gustavobat <gustavoabatistela@gmail.com>
2022-01-09 18:52:50 -0300

    Add level 1 solution

---
+++ 1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# URL: http://www.pythonchallenge.com/pc/def/map.html
+
+import string
+
+
+def shift_text(text, shift):
+    letters = string.ascii_lowercase
+    shifted_letters = letters[shift:] + letters[:shift]
+
+    return text.translate(string.maketrans(letters, shifted_letters))
+
+
+def main():
+    riddle = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq " \
+             "ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq" \
+             " rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+    shift = 2
+
+    riddle_translate = shift_text(riddle, shift)
+    print(riddle_translate)
+
+    solution = shift_text("map", shift)
+    print(solution)
+
+
+if __name__ == "__main__":
+    main()
