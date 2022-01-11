0e1b8ebd1bcf755ddee86da30eec5653fd701a81
gustavobat <gustavoabatistela@gmail.com>
2022-01-11 18:24:24 -0300

    Fetch level 5 input from web

--- 5.py
+++ 5.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 # URL: http://www.pythonchallenge.com/pc/def/peak.html
+
 import pickle
+from urllib.request import urlopen
 
 
 def main():
-    with open('5.in', 'rb') as f:
-        data = pickle.load(f)
+
+    html = urlopen("http://www.pythonchallenge.com/pc/def/banner.p")
+    data = pickle.load(html)
     for line in data:
         print("".join([char * occurrences for char, occurrences in line]))
 
