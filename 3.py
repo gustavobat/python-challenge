28eb4254432bf9d5d0afd550a49a88963ca0468b
gustavobat <gustavoabatistela@gmail.com>
2022-01-11 18:18:12 -0300

    Make level 3 solution cleaner with regex

--- 3.py
+++ 3.py
@@ -6,27 +6,10 @@ import urllib.request
 import re
 
 
-def find_subsequence(query, base):
-    positions = list()
-    for i in range(len(base)):
-        if base[i:i + len(query)] == query:
-            positions.append(i)
-    return positions
-
-
 def main():
-    target = [False, True, True, True, False, True, True, True, False]
-
     html = urllib.request.urlopen("http://www.pythonchallenge.com/pc/def/equality.html").read().decode()
     riddle = re.findall("<!--(.*)-->", html, re.DOTALL)[-1]
-
-    is_upper_case = map(lambda x: True if x.isupper() else False, riddle)
-
-    positions = find_subsequence(target, list(is_upper_case))
-    solution = ""
-    for pos in positions:
-        solution += riddle[pos:pos + len(target)][4]
-    print(solution)
+    print("".join(re.findall(r"[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]", riddle)))
 
 
 if __name__ == "__main__":
